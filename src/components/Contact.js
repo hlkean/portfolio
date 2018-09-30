3232110fc870fd64c16d5d8eba8d63a9da4b89fe
Henry Kean <henrykean@Henrys-MBP.hsd1.nj.comcast.net>
2018-09-30 16:18:33 -0400

    add resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import img from '../images/me.jpg';
+import resume from '../images/Henry_Kean_2018.pdf';
 
 
 export class Contact extends Component {
@@ -22,7 +23,7 @@ export class Contact extends Component {
                     <div className="contact-body">
                         <h2>Reach Out</h2>
                         <p>I'm always looking for new opportunities to work on great projects with awesome people.</p>
-                        <p>Please feel free to reach out by email at <a href="mailto:hlkean@gmail.com">hlkean@gmail.com</a>, or on <a href="https://www.linkedin.com/in/henrykean" target="_blank">LinkedIn</a>.</p>
+                        <p>Please feel free to grab a copy of <a href={resume} download>my résumé</a>, reach out by email at <a href="mailto:hlkean@gmail.com">hlkean@gmail.com</a>, or on <a href="https://www.linkedin.com/in/henrykean" target="_blank">LinkedIn</a>.</p>
                     </div>
                 </div>
             </div>
